3c01c7955d5cc6ba8184ed656dcfce3e75170fda
Diwash Sapkota <53972624+diwashsapkota@users.noreply.github.com>
2020-11-06 22:54:12 +0545

    Uploaded classes and csharp project files

---
+++ MyCalcController.cs
@@ -0,0 +1,41 @@
+﻿using simplecalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simplecalculator.Controllers
+{
+    public class MyCalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new mycalc());
+        }
+
+        [HttpPost]
+        public ActionResult Index(mycalc c, string calculate)
+        {
+            if (calculate == "add")
+            {
+                c.result = c.num1 + c.num2;
+            }
+            else if (calculate == "subt")
+            {
+                c.result = c.num1 - c.num2;
+            }
+            else if (calculate == "mul")
+            {
+                c.result = c.num1 * c.num2;
+            }
+            else
+            {
+                c.result = c.num1 / c.num2;
+            }
+            return View(c);
+        }
+
+    }
+}
---
+++ mycalc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace simplecalculator.Models
+{
+    public class mycalc
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int result { get; set; }
+
+    }
+}
